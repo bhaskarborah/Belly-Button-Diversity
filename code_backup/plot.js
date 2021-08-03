d42a651d79524e3da3a6e275e4db42c7cb265eea
bhaskarborah <bhaskar.borah1984@gmail.com>
2021-08-03 00:11:06 -0700

    Add files via upload

---
+++ code_backup/plot.js
@@ -0,0 +1,10 @@
+d3.json("samples.json").then(function(data){
+    console.log("hello");
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
